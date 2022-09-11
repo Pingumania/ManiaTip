3ec26604a5d59b36b1953cf5f1834b5ac858e689
Pingumania <pingumania@users.noreply.github.com>
2022-09-12 00:10:27 +0200

    Add workaround for PetTracker tooltips

--- core.lua
+++ core.lua
@@ -734,6 +734,13 @@ function mt:ADDON_LOADED(event, addon)
 		cfg = setmetatable(ManiaTipDB, { __index = defaults })
 		ns.cfg = cfg
 	end
+	if PetTrackerMultiTip1 then
+		hooksecurefunc(PetTrackerMultiTip1, "SetScale", function(self, scale)
+			if scale ~= 1 then
+				self:SetScale(1)
+			end
+		end)
+	end
 	if addon == "Blizzard_Communities" then
 		for _, button in pairs(_G.CommunitiesFrame.MemberList.ListScrollFrame.buttons) do
 			button:HookScript("OnEnter", MemberList_OnEnter)
